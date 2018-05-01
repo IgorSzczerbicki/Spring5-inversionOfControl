229279eb592a7d5df9bded1f352ce7b1a512b452
Pl4cek <pl4cek@outlook.com>
2018-05-01 12:35:23 +0200

    V1 - wersja podstawowa MyApp.java odpytuje BaseballCoach.java

---
+++ src/main/java/inversionOfControl/BaseballCoach.java
@@ -0,0 +1,8 @@
+package inversionOfControl;
+
+public class BaseballCoach {
+	
+	public String getDailyWorkout() {
+		return "Plan treningu dla baseballu";
+	}
+}
---
+++ src/main/java/inversionOfControl/MyApp.java
@@ -0,0 +1,10 @@
+package inversionOfControl;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		BaseballCoach theCoach = new BaseballCoach();
+		System.out.println(theCoach.getDailyWorkout());
+	}
+}
